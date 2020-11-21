a8390713d41a796f528c1c63864a6ff48bf55f66
Jargalbat <jargalbat@gmail.com>
2020-11-21 17:19:28 +0800

    first commit

---
+++ src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconficuredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconficuredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "IPhone X",
+                    Summary = "Summary test",
+                    Description = "Desc test",
+                    ImageFile = "priduct-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Samsung S8 plus",
+                    Summary = "Summary test",
+                    Description = "Desc test",
+                    ImageFile = "priduct-2.png",
+                    Price = 840.00M,
+                    Category = "Smart Phone"
+                },
+            };
+        }
+    }
+}
---
+++ src/Catalog/Catalog.API/Settings/ICatalogDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Catalog.API.Settings
+{
+    public interface ICatalogDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public string CollectionName { get; set; }
+    }
+}
