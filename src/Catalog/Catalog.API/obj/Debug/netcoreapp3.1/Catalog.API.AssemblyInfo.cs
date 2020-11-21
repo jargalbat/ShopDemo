87f496aabe281a31bdb1eb7acc0026c63cc92be3
Jargalbat <jargalbat@gmail.com>
2020-11-21 20:03:38 +0800

    changes

--- src/Catalog/Catalog.API/obj/Debug/netcoreapp3.1/Catalog.API.AssemblyInfo.cs
+++ src/Catalog/Catalog.API/obj/Debug/netcoreapp3.1/Catalog.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("5e912176-ae2f-498a-9e60-83259e638160")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
