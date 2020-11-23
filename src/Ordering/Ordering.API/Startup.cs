5b790b33f048b9b679e56873057c5d2c5b4172ad
Jargalbat <jargalbat@gmail.com>
2020-11-23 18:27:29 +0800

    changes

--- src/Ordering/Ordering.API/Startup.cs
+++ src/Ordering/Ordering.API/Startup.cs
@@ -1,17 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Ordering.Application.Handlers;
+using Ordering.Core.Repositories;
+using Ordering.Core.Repositories.Base;
 using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories;
+using Ordering.Infrastructure.Repositories.Base;
 
 namespace Ordering.API
 {
@@ -32,6 +40,41 @@ namespace Ordering.API
             // docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=yourStrong(!)Password' -e 'MSSQL_PID=Express' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu
             services.AddDbContext<OrderContext>(c =>
                 c.UseSqlServer(Configuration.GetConnectionString("OrderConnection")), ServiceLifetime.Singleton);
+
+            services.AddTransient<IOrderRepository, OrderRepository>();
+            services.AddScoped(typeof(IOrderRepository), typeof(OrderRepository));
+            services.AddTransient<IOrderRepository, OrderRepository>();
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddMediatR(typeof(CheckoutOrderHandler).GetTypeInfo().Assembly);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Order API", Version = "v1" });
+            });
+
+            //services.AddSingleton<IRabbitMQConnection>(sp =>
+            //{
+            //    var factory = new ConnectionFactory()
+            //    {
+            //        HostName = Configuration["EventBus:HostName"]
+            //    };
+
+            //    if (!string.IsNullOrEmpty(Configuration["EventBus:UserName"]))
+            //    {
+            //        factory.UserName = Configuration["EventBus:UserName"];
+            //    }
+
+            //    if (!string.IsNullOrEmpty(Configuration["EventBus:Password"]))
+            //    {
+            //        factory.Password = Configuration["EventBus:Password"];
+            //    }
+
+            //    return new RabbitMQConnection(factory);
+            //});
+
+            //services.AddSingleton<EventBusRabbitMQConsumer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,8 +85,6 @@ namespace Ordering.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
@@ -51,6 +92,14 @@ namespace Ordering.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            //app.UseRabbitListener();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API V1");
             });
         }
     }
